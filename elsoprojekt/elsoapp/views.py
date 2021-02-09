7074ef2b3cb45b9b755c266cab3d1a587c0ad8f5
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2021-02-09 11:28:22 +0100

    első lyukas html (template)

--- elsoprojekt/elsoapp/views.py
+++ elsoprojekt/elsoapp/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import render
 
 def home_view(request, *args, **kwargs):
 	kontextus = {
-	"a": 123,
-	"b": "blablabla",
-	"l": [1,3,5,7,9],
+		"a": 123,
+		"b": "blablabla",
+		"l": [1,3,5,7,9],
 	}
+
+	print(f"A kontextusban az a értéke: {kontextus['a']}")
+
+
 	return render(request, "home.html", kontextus) 
