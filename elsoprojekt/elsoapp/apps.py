c0cc7e08e98579f2ffc30e055a075be986d5b7fa
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2021-02-09 11:00:54 +0100

    első projekt és view létrehozása

---
+++ elsoprojekt/elsoapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ElsoappConfig(AppConfig):
+    name = 'elsoapp'
---
+++ elsoprojekt/elsoapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home_view(request, *args, **kwargs):
+	kontextus = {
+	"a": 123,
+	"b": "blablabla",
+	"l": [1,3,5,7,9],
+	}
+	return render(request, "home.html", kontextus) 
