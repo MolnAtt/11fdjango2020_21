e8fd62161f49c041d2a454ed4085cebd76f397b0
MolnarAttila <MolnarAttila@tan-nt1.tanari.szlgbp.local>
2021-02-10 12:05:43 +0100

    első modellek, admin szerkeszthet Tanulot

--- elsoprojekt/elsoapp/admin.py
+++ elsoprojekt/elsoapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Tanulo
+
+admin.site.register(Tanulo)
--- elsoprojekt/elsoapp/models.py
+++ elsoprojekt/elsoapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tanulo(models.Model):
+
+	nev = models.CharField(max_length=128)
+	fnev = models.CharField(max_length=128)
+	jelszo = models.CharField(max_length=128)
+	email = models.CharField(max_length=128)
+
+class Foglalkozas(models.Model):
+	nev = models.CharField(max_length=128)
+	
+class Valasztas(models.Model):
+	tanulo = models.ForeignKey(Tanulo, on_delete=models.CASCADE)
+	foglalkozas = models.ForeignKey(Foglalkozas, on_delete=models.CASCADE)
